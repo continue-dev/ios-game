f7a877f6959edf2c35acd07b83521eb2d5923004
izumi_yoshiki <izumiyoshiki@icloud.com>
2019-11-30 18:25:09 +0900

    add font

--- slokara/slokara/Title/TitleViewController.swift
+++ slokara/slokara/Title/TitleViewController.swift
@@ -5,6 +5,13 @@ class TitleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let label = UILabel()
+        label.text = "あいう夏スロ異世界！！\n★123１２３";
+        label.frame = CGRect(x:10, y:10, width:400, height:200);
+        label.font = UIFont(name: "07LogoTypeGothic7" , size: 32)
+        label.numberOfLines = 2
+        view.addSubview(label)
+
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
