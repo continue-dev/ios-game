fe0e4e11d39c76555d3bfc97b043f8d9a2ff90c4
izumi_yoshiki <izumiyoshiki@icloud.com>
2019-12-18 19:21:08 +0900

    add tasklist

---
+++ slokara/slokara/TaskList/TaskListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListViewController.swift
+//  slokara
+//
+//  Created by Yoshiki Izumi on 2019/12/18.
+//  Copyright © 2019 Sab_swiftlin. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class TaskListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
