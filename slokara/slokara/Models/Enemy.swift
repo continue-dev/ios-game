1d7f1cd33b694e506fd6d888d10778be409cf19e
Sab-swiftlin <nukunuku23@gmail.com>
2020-02-23 23:31:10 +0900

    バトル画面の実装に必要そうなModelを追加

---
+++ slokara/slokara/Models/Enemy.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+struct Enemy {
+    let id: Int
+    let name: String
+    let hp: Int
+    let attack: Int
+    let defense: Int
+    let attackType: [AttributeType]
+    let defenseType: [AttributeType]
+    let image: UIImage
+}
+
