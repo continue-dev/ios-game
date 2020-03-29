e6fd943fb8b4a87ea16927a481f44ef34a27c29d
Sab-swiftlin <nukunuku23@gmail.com>
2020-03-29 15:02:59 +0900

    ランクアップメソッドを追加

--- slokara/slokara/RealmObjects/UserStatus.swift
+++ slokara/slokara/RealmObjects/UserStatus.swift
@@ -13,4 +13,43 @@ class UserStatus: Object {
         set { gradeString = newValue.rawValue }
         get { return Grade(rawValue: gradeString)! }
     }
+    
+    func rankUp() {
+        switch grade {
+        case .wood:
+            if rankValue >= 3 {
+                rankValue = 1
+                grade = .stone
+            } else {
+                rankValue += 1
+            }
+        case .stone:
+            if rankValue >= 3 {
+                rankValue += 1
+                grade = .copper
+            } else {
+                rankValue += 1
+            }
+        case .copper:
+            if rankValue >= 6 {
+                rankValue = 1
+                grade = .silver
+            } else {
+                rankValue += 1
+            }
+        case .silver:
+            if rankValue >= 3 {
+                rankValue = 1
+                grade = .gold
+            } else {
+                rankValue += 1
+            }
+        case .gold:
+            if rankValue >= 3 {
+                return
+            } else {
+                rankValue += 1
+            }
+        }
+    }
 }
