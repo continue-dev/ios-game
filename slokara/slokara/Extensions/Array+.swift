395d8704dfd9feedf6e33382938851ebd0847244
Sab-swiftlin <nukunuku23@gmail.com>
2020-04-23 01:00:23 +0900

    Arrayに任意の数で分割するExtentionを追加

--- slokara/slokara/Extensions/Array+.swift
+++ slokara/slokara/Extensions/Array+.swift
@@ -5,3 +5,15 @@ extension Array where Element: Hashable {
         return Set(indexes.map { self[$0] }).count == 1
     }
 }
+
+extension Array {
+    func separate(of size: Int) -> [[Element]] {
+        var selfArray = self
+        var result = [[Element]]()
+        while selfArray.count > 0 {
+            result.append(Array(selfArray.prefix(size)))
+            selfArray = selfArray.suffix(selfArray.count - size)
+        }
+        return result
+    }
+}
