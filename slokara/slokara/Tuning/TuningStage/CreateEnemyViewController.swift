0aaffcc89c8dd6c92d2a26b5e6ee434090d698fc
Sab-swiftlin <nukunuku23@gmail.com>
2020-04-29 04:11:12 +0900

    敵キャラ設定画面のベースを作成

---
+++ slokara/slokara/Tuning/TuningStage/CreateEnemyViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class CreateEnemyViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
---
+++ slokara/slokara/Tuning/TuningStage/ToggleAttributeImageView.swift
@@ -0,0 +1,26 @@
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ToggleAttributeImageView: UIImageView {
+    private var attribute: AttributeType! {
+        didSet{ self.image = attribute.image }
+    }
+    
+    private var isSelected: Bool = false {
+        didSet{ self.alpha = isSelected ? 1 : 0.3 }
+    }
+    
+    private let selectEvent = PublishRelay<Bool>()
+    var didTap: Observable<Bool> { return selectEvent.asObservable() }
+    
+    func setAttribute(attribute: AttributeType) {
+        self.attribute = attribute
+        self.alpha = 0.3
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.isSelected.toggle()
+        self.selectEvent.accept(self.isSelected)
+    }
+}
