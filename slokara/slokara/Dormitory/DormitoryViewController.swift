c462d0a7c3d24f7469f0825323e7d90eba7bdbdf
Sab-swiftlin <nukunuku23@gmail.com>
2020-05-05 22:21:51 +0900

    寮の画面のレイアウトを修正

--- slokara/slokara/Dormitory/DormitoryViewController.swift
+++ slokara/slokara/Dormitory/DormitoryViewController.swift
@@ -1,30 +1,38 @@
 import UIKit
+import RxSwift
+import RxCocoa
 
 class DormitoryViewController: UIViewController, NavigationChildViewController {
     @IBOutlet weak var topSpacer: UIView!
-    @IBOutlet weak var statusButton: UIButton!
-    @IBOutlet weak var itemButton: UIButton!
+    @IBOutlet weak var statusButton: BorderedButton!
+    @IBOutlet weak var itemButton: BorderedButton!
     
-    private let fontName = "07LogoTypeGothic7"
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        applyButtonTitleFont()
+        bind()
     }
     
-    private func applyButtonTitleFont() {
-        self.statusButton.titleLabel?.font = UIFont(name: self.fontName, size: self.statusButton.titleLabel!.font.pointSize)
-        self.itemButton.titleLabel?.font = UIFont(name: self.fontName, size: self.itemButton.titleLabel!.font.pointSize)
+    private func bind() {
+        self.statusButton.buttonTapped.bind(to: transitionToStatus).disposed(by: disposeBag)
+        self.itemButton.buttonTapped.bind(to: transitionToItem).disposed(by: disposeBag)
+    }
+}
+
+extension DormitoryViewController {
+    private var transitionToStatus: Binder<Void> {
+        return Binder(self) { me, _ in
+            let navigation = me.parent as! NavigationViewController
+            let parameterVC = UIStoryboard(name: "Parameter", bundle: nil).instantiateInitialViewController() as! NavigationChildViewController
+            parameterVC.title = "ステータス"
+            navigation.push(parameterVC, animate: true)
+        }
     }
     
-    @IBAction func statusButtonTapped(_ sender: Any) {
-        let navigation = self.parent as! NavigationViewController
-        let parameterVC = UIStoryboard(name: "Parameter", bundle: nil).instantiateInitialViewController() as! NavigationChildViewController
-        parameterVC.title = "ステータス"
-        navigation.push(parameterVC, animate: true)
-    }
-    
-    @IBAction func itemButtonTapped(_ sender: Any) {
-        // アイテム画面へ遷移
+    private var transitionToItem: Binder<Void> {
+        return Binder(self) { me, _ in
+            // アイテム画面へ遷移
+        }
     }
 }
