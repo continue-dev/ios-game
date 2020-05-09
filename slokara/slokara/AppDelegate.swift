0b25c5823dfa78990036d883e673eaf013a357a9
Sab-swiftlin <nukunuku23@gmail.com>
2020-05-09 17:04:58 +0900

    Realmのマイグレーション処理のベースを追加

--- slokara/slokara/AppDelegate.swift
+++ slokara/slokara/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,8 +8,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        migrateRealm()
         return true
     }
+    
+    private func migrateRealm() {
+        let schemaVersion: UInt64 = 0
 
+        let config = Realm.Configuration(
+            schemaVersion: schemaVersion,
+            migrationBlock: { migration, oldSchemaVersion in
+                if (oldSchemaVersion < schemaVersion) {
+                    // Do Migration
+                }
+        })
+        Realm.Configuration.defaultConfiguration = config
+    }
+    
 }
 
