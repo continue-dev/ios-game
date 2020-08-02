f5baf582c5add09b4af6034d271c7e42d4228e5a
Sab-swiftlin <nukunuku23@gmail.com>
2020-08-02 23:28:59 +0900

    購買部画面を追加

---
+++ slokara/slokara/Shop/ShopViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ShopViewController: UIViewController, NavigationChildViewController {
+    @IBOutlet weak var topSpacer: UIView!
+    @IBOutlet weak var itemShopButton: BorderedButton!
+    @IBOutlet weak var reelShopButton: BorderedButton!
+    @IBOutlet weak var adButton: BorderedButton!
+    @IBOutlet weak var talkButton: BorderedButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
