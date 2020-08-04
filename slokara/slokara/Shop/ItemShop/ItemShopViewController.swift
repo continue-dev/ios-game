c10afe018af028cfc071d3e2f78d97132fe7955c
Sab-swiftlin <nukunuku23@gmail.com>
2020-08-05 04:03:48 +0900

    アイテムショップ画面の基本要素を配置

--- slokara/slokara/Shop/ItemShop/ItemShopViewController.swift
+++ slokara/slokara/Shop/ItemShop/ItemShopViewController.swift
@@ -1,1 +1,11 @@
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ItemShopViewController: UIViewController, NavigationChildViewController {
+    @IBOutlet weak var topSpacer: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
