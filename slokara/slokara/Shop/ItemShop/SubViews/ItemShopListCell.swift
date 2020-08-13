4b100cd89162e70654e5bb97b661f94f9ca0e501
Sab-swiftlin <nukunuku23@gmail.com>
2020-08-13 16:25:55 +0900

    アイテム一覧用のセルをアップデート

--- slokara/slokara/Shop/ItemShop/SubViews/ItemShopListCell.swift
+++ slokara/slokara/Shop/ItemShop/SubViews/ItemShopListCell.swift
@@ -10,12 +10,13 @@ class ItemShopListCell: UITableViewCell {
     @IBOutlet private weak var choosingLayerImageView: UIImageView!
     
     private let selectedBorderColor = UIColor(red: 36/255, green: 109/255, blue: 81/255, alpha: 1)
+    private let purchaseBorderColor = UIColor(red: 238/255, green: 17/255, blue: 25/255, alpha: 1)
     
-    var purchaseNumber = 0
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    var purchaseNumber = 0 {
+        didSet {
+            purchaseNumberLabel.text = "+\(purchaseNumber)"
+            applyPurchaseBorderColor()
+        }
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -29,4 +30,30 @@ class ItemShopListCell: UITableViewCell {
         }
     }
     
+    func setItem(item: Item) {
+        iconImageView.image = item.icon
+        itemNameLabel.text = item.name
+        priceLabel.text = "\(item.price)"
+    }
+    
+    func setPossession(number: Int) {
+        possessionNumberLabel.text = "\(number)"
+    }
+    
+    func incrementPurchaseNumber() {
+        purchaseNumber += 1
+    }
+    
+    func decrementPurchaseNumber() {
+        guard purchaseNumber > 0 else { return }
+        purchaseNumber -= 1
+    }
+    
+    private func applyPurchaseBorderColor() {
+        if purchaseNumber > 0 {
+            purchaseNumberLabel.strokeColor = purchaseBorderColor
+        } else {
+            purchaseNumberLabel.strokeColor = self.isSelected ? selectedBorderColor : .black
+        }
+    }
 }
