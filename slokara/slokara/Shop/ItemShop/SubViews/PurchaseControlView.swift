23c7fd8aa6872b10727573657e78ef14069c3e8b
Sab-swiftlin <nukunuku23@gmail.com>
2020-08-14 20:42:02 +0900

    購入数を操作するカスタムビューを作成

---
+++ slokara/slokara/Shop/ItemShop/SubViews/PurchaseControlView.swift
@@ -0,0 +1,61 @@
+import UIKit
+import RxSwift
+import RxCocoa
+
+class PurchaseControlView: UIView {
+    @IBOutlet private weak var itemImageView: UIImageView!
+    @IBOutlet private weak var itemInfoLabel: CommonFontLabel!
+    @IBOutlet private weak var purchaseNumberLabel: CommonFontLabel!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
+    
+    private let purchaseNumberRelay = BehaviorRelay<Int>(value: 0)
+    var purchaseNumber: Observable<Int> {
+        return purchaseNumberRelay.asObservable()
+    }
+    
+    private let downSwipeRelay = PublishRelay<Void>()
+    var hideViewEvent: Observable<Void> {
+        return downSwipeRelay.asObservable()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadNib()
+    }
+    
+    private func loadNib() {
+        guard let customView = Bundle.main.loadNibNamed("PurchaseControlView", owner: self, options: nil)?.first as? UIView else { return }
+        customView.frame = self.bounds
+        customView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        self.addSubview(customView)
+    }
+    
+    func setItemListModel(model: ItemListModel) {
+        itemImageView.image = model.item.image
+        itemInfoLabel.text = model.item.info
+        purchaseNumberRelay.accept(model.possessionNumber)
+    }
+    
+    @IBAction private func plusButtonTapped(_ sender: Any) {
+        let purchaseNumber = purchaseNumberRelay.value
+        purchaseNumberLabel.text = "\(purchaseNumber + 1)"
+        purchaseNumberRelay.accept(purchaseNumber + 1)
+    }
+    
+    @IBAction private func minusButtonTapped(_ sender: Any) {
+        let purchaseNumber = purchaseNumberRelay.value
+        guard purchaseNumber > 0 else { return }
+        purchaseNumberLabel.text = "\(purchaseNumber - 1)"
+        purchaseNumberRelay.accept(purchaseNumber - 1)
+    }
+    
+    @IBAction private func swipeAction(_ sender: Any) {
+        downSwipeRelay.accept(())
+    }
+}
