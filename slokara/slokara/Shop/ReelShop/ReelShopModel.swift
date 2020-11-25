dc08dac3412bd6940fe30fc5444eddcf4eecae29
Sab-swiftlin <nukunuku23@gmail.com>
2020-11-25 21:55:11 +0900

    リール購入画面のファイルを追加

---
+++ slokara/slokara/Shop/ReelShop/ReelShopModel.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+protocol ReelShopModelProtocol {
+    
+}
+
+final class ReelShopModelImpl: ReelShopModelProtocol {
+    
+}
---
+++ slokara/slokara/Shop/ReelShop/ReelShopViewController.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+class ReelShopViewController: UIViewController, NavigationChildViewController {
+    
+}
