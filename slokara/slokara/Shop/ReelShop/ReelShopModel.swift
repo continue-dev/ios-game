b44345cc0c74ad860ba2ad463dde536e00a4561e
Sab-swiftlin <nukunuku23@gmail.com>
2021-01-23 20:33:07 +0900

    チューニングモードの動作を追加

--- slokara/slokara/Shop/ReelShop/ReelShopModel.swift
+++ slokara/slokara/Shop/ReelShop/ReelShopModel.swift
@@ -17,10 +17,26 @@ final class ReelShopModelImpl: ReelShopModelProtocol {
         guard let realm = try? Realm() else { assert(false, "Realmをインスタンス化できませんでした") }
         guard let status = realm.objects(UserStatus.self).first else { assert(false, "ユーザーステータスを取得できませんでした") }
         self.status = status
+        
+        #if !PROD
+        if UserDefaults.standard.bool(forKey: "tuningMode") {
+            self.status = realm.objects(UserStatus.self)[1]
+        }
+        #endif
+        
         return Observable.just(status.numberOfCoins)
     }
     
     var currentReel: Observable<Reel> {
+        #if !PROD
+        if UserDefaults.standard.bool(forKey: "tuningMode") {
+            guard let json =  UserDefaults.standard.data(forKey: "reel") else {
+                return Observable.just(Reel(top: [false, false, false], center: [false, true, false], bottom: [false, false, false]))
+            }
+            guard let debugReel = try? JSONDecoder().decode(Reel.self, from: json) else { fatalError("Reel decode failed.") }
+            return Observable.just(debugReel)
+        }
+        #endif
         guard let realm = try? Realm() else { assert(false, "Realmをインスタンス化できませんでした") }
         guard let reel = realm.objects(ReelStatus.self).first else { assert(false, "リールステータスを取得できませんでした") }
         self.reel = reel
@@ -35,6 +51,14 @@ final class ReelShopModelImpl: ReelShopModelProtocol {
     }
     
     func saveFutureReel(_ reel: Reel) {
+        #if !PROD
+        if UserDefaults.standard.bool(forKey: "tuningMode") {
+            guard let json = try? JSONEncoder().encode(reel) else { fatalError("Reel encode failed.") }
+            UserDefaults.standard.set(json, forKey: "reel")
+            return
+        }
+        #endif
+        
         guard let realm = try? Realm() else { assert(false, "Realmをインスタンス化できませんでした") }
         try! realm.write {
             self.reel.topLeft = reel.top[0]
