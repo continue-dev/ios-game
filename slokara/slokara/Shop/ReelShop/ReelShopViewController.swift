b9634126f183b9cbed284fa34e6080c234ae1524
Sab-swiftlin <nukunuku23@gmail.com>
2021-01-23 17:53:38 +0900

    リールタップで金額が反映されるように追加

--- slokara/slokara/Shop/ReelShop/ReelShopViewController.swift
+++ slokara/slokara/Shop/ReelShop/ReelShopViewController.swift
@@ -1,4 +1,6 @@
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ReelShopViewController: UIViewController, NavigationChildViewController {
     @IBOutlet weak var topSpacer: UIView!
@@ -7,11 +9,36 @@ class ReelShopViewController: UIViewController, NavigationChildViewController {
     @IBOutlet private weak var paymentCoinsLabel: BorderedLabel!
     @IBOutlet private weak var shoppingView: ReelShoppingView!
     
+    private let disposeBag = DisposeBag()
+    private lazy var viewModel = ReelShopViewModel(reelStatus: shoppingView.reelStateObservable)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        shoppingView.setReelStatus(reel: Reel(top: [false, false, true], center: [false, true, false], bottom: [true, false, false]))
+        bind()
+    }
+    
+    private func bind() {
+        viewModel.currentCoins.map { "\($0)" }.bind(to: currentCoinsLabel.rx.text).disposed(by: disposeBag)
+        viewModel.paymentCoins.map { "\($0)" }.bind(to: paymentCoinsLabel.rx.text).disposed(by: disposeBag)
+        viewModel.currentReel.subscribe(onNext: { [weak self] reel in
+            self?.shoppingView.setReelStatus(reel: reel)
+        }).disposed(by: disposeBag)
+        
+        Observable.combineLatest(viewModel.currentCoins, viewModel.paymentCoins)
+            .map { current, payment in
+                payment <= current
+            }
+            .bind(to: canSopping)
+            .disposed(by: disposeBag)
+    }
+}
 
+extension ReelShopViewController {
+    private var canSopping: Binder<Bool> {
+        return Binder(self) { me, can in
+            me.paymentCoinsLabel.textColor = can ? .white : .red
+            me.enterButton.isHidden = !can
+        }
     }
 }
--- slokara/slokara/Shop/ReelShop/ReelShopViewModel.swift
+++ slokara/slokara/Shop/ReelShop/ReelShopViewModel.swift
@@ -1,5 +1,30 @@
 import Foundation
+import RxSwift
 
 final class ReelShopViewModel {
+    private let model: ReelShopModelProtocol
+    private let disposeBag = DisposeBag()
     
+    // リール購入ベース価格
+    private let reelPrice = 500
+    
+    var currentCoins: Observable<Int>
+    private let paymentCoinsRelay = BehaviorSubject(value: 0)
+    var paymentCoins: Observable<Int> {
+        return paymentCoinsRelay.asObservable()
+    }
+    
+    var currentReel: Observable<Reel>
+    
+    init(reelStatus: Observable<[[ReelStatusView.ShopReelStatus]]>, reelShopModel: ReelShopModelProtocol = ReelShopModelImpl()) {
+        model = reelShopModel
+        
+        currentCoins = model.currentCoins
+        currentReel = model.currentReel
+        reelStatus.subscribe(onNext: { [unowned self] status in
+            let selectedCount = status.flatMap { $0 }.filter { $0 == .selected }.count
+            let price = reelPrice * (0...selectedCount).reduce(0) { $0 + $1 }
+            paymentCoinsRelay.onNext(price)
+        }).disposed(by: disposeBag)
+    }
 }
