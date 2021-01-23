49639edd1240991afadeb30cfc6acbfa1a00112e
Sab-swiftlin <nukunuku23@gmail.com>
2021-01-23 16:12:38 +0900

    ViewControllerのリール表示部分をカスタムビューに変更

--- slokara/slokara/Shop/ReelShop/ReelShopViewController.swift
+++ slokara/slokara/Shop/ReelShop/ReelShopViewController.swift
@@ -1,7 +1,17 @@
 import UIKit
 
 class ReelShopViewController: UIViewController, NavigationChildViewController {
-    @IBOutlet var topSpacer: UIView!
+    @IBOutlet weak var topSpacer: UIView!
+    @IBOutlet private weak var enterButton: BorderedButton!
+    @IBOutlet private weak var currentCoinsLabel: BorderedLabel!
+    @IBOutlet private weak var paymentCoinsLabel: BorderedLabel!
+    @IBOutlet private weak var shoppingView: ReelShoppingView!
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        shoppingView.setReelStatus(reel: Reel(top: [false, false, true], center: [false, true, false], bottom: [true, false, false]))
+
+    }
 }
